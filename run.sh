f43ebdc945caa216555dcc2ed2e820cbdb81513d
aryelciu001 <aryelciu001@gmail.com>
2021-10-11 18:33:00 +0800

    ADD: runnable file

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+sudo yum-config-manager --enable epel*
+sudo yum install -y certbot
+docker-compose up -d
+sudo certbot
