5d210bdac7e03422c0612b9a7f1cb4d1941ebb38
aryelciu001 <aryelciu001@gmail.com>
2021-10-11 18:46:38 +0800

    FIX: bug, placement

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.r
 sudo yum-config-manager --enable epel*
 sudo yum install -y certbot
 docker-compose up -d
-sudo certbot
+sudo certbot certonly
